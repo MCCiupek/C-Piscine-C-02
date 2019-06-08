8bac2620001b2fae2539bbc4d13861d0d3e157a6
MCCiupek <51337012+MCCiupek@users.noreply.github.com>
2019-06-09 00:36:42 +0200

    Add files via upload

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -1,38 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mciupek <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/08 21:03:47 by mciupek           #+#    #+#             */
-/*   Updated: 2019/06/09 00:27:44 by mciupek          ###   ########.fr       */
+/*   Created: 2019/06/09 00:17:31 by mciupek           #+#    #+#             */
+/*   Updated: 2019/06/09 00:32:51 by mciupek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-char	*ft_strcpy(char *dest, char *src)
+char *ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	int i;
-	
+			
 	i = 0;
 
-	while (src[i] != '\0')
+	while (i <= n)
 	{
-		dest[i] = src[i];
+		if (src[i] == '\0')
+			dest[i] = '\0';	
+		else
+			dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
 	return dest;
-}
-
-int		main(void)
-{
-	char dest[3];
-	char *src;
-	
-	src = "Hello";
-	ft_strcpy(dest, src);
-	printf("%s", dest);
 }
